0e4ff6c2ef2ba1fc048e0725bd8290fabe61c42f
minkere <minkere715@gmail.com>
2025-11-17 21:37:25 -0800

    Added Top Ten Player js file

---
+++ js/sketches/viz/viz_toptenplayers.js
@@ -0,0 +1,10 @@
+// viz_toptenplayers.js
+(function () {
+    window.VizTopTenPLayers = {
+        draw: function (p, manager, ai, progress) {
+            var cx = (manager.offsetX || 0) + (manager.width || 600) / 2;
+            var cy = (manager.offsetY || 0) + (manager.height || 520) / 3;
+
+        }
+    };
+})();

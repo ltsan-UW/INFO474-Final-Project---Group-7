58fa3e58fc2a184bef1d8573ed256ee95bccc6af
Diego Licea <dlicea@uw.edu>
2025-11-17 21:00:03 -0800

    Added .js files for line graph and line graph zoomed

---
+++ js/sketches/viz/line_graph_full.js
@@ -0,0 +1,11 @@
+// viz_title.js
+// Draw title-style screens for early active indexes (0 and 1)
+(function () {
+    window.VizTitle = {
+        draw: function (p, manager, ai, progress) {
+            var cx = (manager.offsetX || 0) + (manager.width || 600) / 2;
+            var cy = (manager.offsetY || 0) + (manager.height || 520) / 3;
+
+        }
+    };
+})();

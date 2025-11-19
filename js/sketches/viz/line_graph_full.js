8ea98421afd15b33911109569418f5b2c264c95c
Diego Licea <diegol@Diegos-MacBook-Air-9.local>
2025-11-18 18:32:51 -0800

    Added preload logic to lineGraph

--- js/sketches/viz/line_graph_full.js
+++ js/sketches/viz/line_graph_full.js
@@ -3,14 +3,36 @@
 (function () {
     window.VizLineGraphFull = {
 
+        doneLoading: false,
 
         preload: function(manager){
             let playerCounts = {};
+            console.log("hello world");
 
+            for (let season in manager.data){
+                let usaCount = 0; 
+                let interCount = 0;
+                let curSeason = manager.data[season];
+                for (let player in curSeason){
+                    let playerObj = curSeason[player];
+                    if (playerObj.country !== "USA"){
+                        interCount++;
+                    } else {
+                        usaCount++;
+                    }
+                }
+            }
+
+            this.doneLoading = true;
             //i will write the pre processing here
         },
 
         draw: function (p, manager, ai, progress) {
+            if (!this.doneLoading){
+                VizLineGraphFull.preload(manager);
+            }
+            
+            console.log("Yo");
             var cx = (manager.offsetX || 0) + (manager.width || 600) / 2;
             var cy = (manager.offsetY || 0) + (manager.height || 520) / 3;
 
@@ -18,6 +40,7 @@
 
             p.textSize(20);
             p.text("Testing", cx, cy);
+            
         }
     };
 })();

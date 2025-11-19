7db34b5e41f23dfb6fad9692a7fdad22f307455e
Diego Licea <dlicea@uw.edu>
2025-11-18 21:25:19 -0800

    Made a bar plot for testing, will convert into line graph

--- js/sketches/viz/line_graph_full.js
+++ js/sketches/viz/line_graph_full.js
@@ -4,11 +4,11 @@
     window.VizLineGraphFull = {
 
         doneLoading: false,
+        playerCounts: [],
 
         preload: function(manager){
-            let playerCounts = {};
-            console.log("hello world");
 
+            //Find the % of international players for all seasons
             for (let season in manager.data){
                 let usaCount = 0; 
                 let interCount = 0;
@@ -21,26 +21,65 @@
                         usaCount++;
                     }
                 }
+                let total = interCount + usaCount;
+                let interPerc = interCount/total;
+                this.playerCounts.push({
+                    Season: season,
+                    Percentage: interPerc.toFixed(2)
+                })
             }
 
+            for (let i = 0; i < 4; i++){
+                let current = this.playerCounts.pop();
+                this.playerCounts.unshift(current);
+            }
+            console.log(this.playerCounts);
             this.doneLoading = true;
-            //i will write the pre processing here
         },
+
+        //rgb(209, 13, 13)) <---- rgb color picker
 
         draw: function (p, manager, ai, progress) {
             if (!this.doneLoading){
                 VizLineGraphFull.preload(manager);
             }
-            
-            console.log("Yo");
+
             var cx = (manager.offsetX || 0) + (manager.width || 600) / 2;
             var cy = (manager.offsetY || 0) + (manager.height || 520) / 3;
 
             p.fill("black");
 
             p.textSize(20);
-            p.text("Testing", cx, cy);
-            
+
+            // STEP 1. BORDERS 
+            p.push()
+            p.strokeCap(p.SQUARE);
+            p.stroke(187, 187, 187);
+            p.strokeWeight(2);
+            p.line(cx - 320, cy-100, cx - 320, cy + 320);
+            p.line(cx-320,cy + 320, cx + 260, cy + 320);
+            p.pop()
+
+            //STEP 2. DEFINING "TICK" LOCATIONS
+            p.push()
+            p.strokeCap(p.SQUARE);
+            p.stroke(209, 13, 13);
+            p.strokeWeight(3);
+
+            // 640 / 28
+            let pos = cx-320 + 20.6;
+            let base = cy+320;
+            //let previousX = 0;
+            //let previousY = 0;
+            for (let i = 0; i < this.playerCounts.length; i++) {
+                let obj = this.playerCounts[i];
+                let percentage = (420) * obj.Percentage;
+                p.line(pos, base - percentage, pos, base);
+                pos += 20.6;
+            }
+            p.pop();
+
+
         }
     };
 })();

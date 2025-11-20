8c7a2ae2ca3acb36d3c974b1838d003cbc45d6b2
ltsan-UW <ltsan30@uw.edu>
2025-11-20 15:19:31 -0800

    Added hover over player shows the name in rectangle, moved make circle positions into preload all in all players viz

--- js/sketches/viz/viz_allplayers.js
+++ js/sketches/viz/viz_allplayers.js
@@ -2,24 +2,59 @@
 
     window.VisAllPlayers = {
         doneLoading: false,
-        seed: null,
-        seasonData: null,
+        maxPlayers: 0,
         currentSeason: null,
+        circles: null,
+        mouseClick: false,
+        clickedCircle: null,
 
         preload: function(manager) {
             let testSeasons = ["1996-97", "2015-16", "2020-21", "2021-22"];
             this.currentSeason = testSeasons[2] // will change to be determined by manager
-            this.seed = Math.random() * 10; // can be set to static
-            this.seasonData = manager.data[this.currentSeason];
+            let seasonData = manager.data[this.currentSeason];
             this.doneLoading = true;
+            let playerNames = Object.keys(seasonData);
+            this.maxPlayers = Object.keys(seasonData).length;
+            this.circles = {}
+            let bigRadius = manager.height * 0.46 - 2;
+            let midX = (manager.offsetX || 0) + (manager.width || 600) / 2;
+            let midY = (manager.offsetY || 0) + (manager.height || 520) / 2 + 40;
+
+            let maxSpacing = Math.sqrt(Math.PI * bigRadius * bigRadius / this.maxPlayers);
+            let r = maxSpacing * 0.7;
+            let spacing = maxSpacing;
+            let maxCountRows = Math.floor(bigRadius * 2 / spacing) //get the max amount of rows possible with spacing
+            let minSpacingY = bigRadius * 2 / maxCountRows;
+            let gap = spacing - r;
+            let count = 0;
+            for(let row = 0; row <= maxCountRows; row++) {
+                let y = minSpacingY / 2 + row * minSpacingY - bigRadius;
+                let maxX = Math.sqrt(bigRadius * bigRadius - y * y);
+                let maxCountCols = Math.floor(maxX * 2 / spacing);
+                let minSpacingX = (maxCountCols != 0 ? maxX * 2 / maxCountCols : 0);
+                for(let i = 0; i <= maxCountCols; i++) {
+                    if(count + 1 > this.maxPlayers) break;
+                    this.circles[playerNames[count]] = {
+                        x: midX - maxX + (minSpacingX * i) + (Math.random() * gap * 2 - gap),
+                        y: midY - y + (Math.random() * gap * 2 - gap),
+                        r: r,
+                        VORP: seasonData[playerNames[count]].VORP,
+                        name: seasonData[playerNames[count]].name
+                    };
+                    count++;
+                }
+            }
+
+            //written with AI, I didn't wanna mess with using a onClick function and going through the instance stuff
+            document.querySelector('canvas').addEventListener('click', (e) => {
+                this.mouseClick = !this.mouseClick;
+            });
         },
 
         draw: function (p, manager, ai, progress) {
             if(!this.doneLoading) {
                 VisAllPlayers.preload(manager);
             }
-
-            let maxPlayers = Object.keys(this.seasonData).length;
 
             p.noStroke();
             p.fill('black');
@@ -29,23 +64,20 @@
             p.text('NBA Season ' + this.currentSeason, manager.offsetX + 5, manager.offsetY + 35);
             p.textSize(18);
             p.textStyle(p.NORMAL);
-            p.text('Total Players: ' + maxPlayers, manager.offsetX + 5, manager.offsetY + 55);
+            p.text('Total Players: ' + this.maxPlayers, manager.offsetX + 5, manager.offsetY + 55);
 
 
-            let bigRadius = manager.height * 0.46 - 2;
             // p.circle(manager.offsetX + manager.width / 2, manager.offsetY + manager.height / 2, bigRadius * 2);
 
-            let midX = (manager.offsetX || 0) + (manager.width || 600) / 2;
-            let midY = (manager.offsetY || 0) + (manager.height || 520) / 2 + 40;
             // p.line(xStart, yEnd, xStart, yStart);
             // p.line(xEnd, yEnd, xEnd, yStart);
 
             //let pixelsToMeter = xDistance / 13.42;
 
-            let yStart = midY - bigRadius;
-            let yEnd = midY + bigRadius + 20;
-            let xStart = manager.offsetX;
-            let xEnd = manager.offsetX + manager.width;
+            // let yStart = midY - bigRadius;
+            // let yEnd = midY + bigRadius + 20;
+            // let xStart = manager.offsetX;
+            // let xEnd = manager.offsetX + manager.width;
 
             // let xDistance = xEnd - xStart;
             // let pixelsToMeter = xDistance / 15;
@@ -69,30 +101,62 @@
             p.strokeWeight(1);
             p.stroke('grey')
             p.fill('lightgrey');
-            let maxSpacing = Math.sqrt(p.PI * bigRadius * bigRadius / maxPlayers);
-            let r = maxSpacing * 0.7;
-            let spacing = maxSpacing;
-            let maxCountRows = Math.floor(bigRadius * 2 / spacing) //get the max amount of rows possible with spacing
-            let minSpacingY = bigRadius * 2 / maxCountRows;
-            p.randomSeed(this.seed);
-            let gap = spacing - r;
-            let count = 0;
-            for(let row = 0; row <= maxCountRows; row++) {
-                let y = minSpacingY / 2 + row * minSpacingY - bigRadius;
-                let maxX = Math.sqrt(bigRadius * bigRadius - y * y);
-                let maxCountCols = Math.floor(maxX * 2 / spacing);
-                let minSpacingX = (maxCountCols != 0 ? maxX * 2 / maxCountCols : 0);
-                for(let i = 0; i <= maxCountCols; i++) {
-                    if(count + 1 > maxPlayers) break;
-                    p.circle(midX - maxX + (minSpacingX * i) + p.random(-gap, gap), midY - y + p.random(-gap, gap), r);
-                    count++;
+
+            for(let circle in this.circles) {
+                let playerCircle = this.circles[circle];
+                p.circle(playerCircle.x, playerCircle.y, playerCircle.r);
+                if (p.dist(p.mouseX, p.mouseY, playerCircle.x, playerCircle.y) < (playerCircle.r / 2 + 5)) {
+                    p.fill('red')
+                    this.clickedCircle = playerCircle;
+                    p.circle(playerCircle.x, playerCircle.y, playerCircle.r + 5);
+                    p.fill('lightgrey');
                 }
             }
 
+
+            p.fill('white');
+            p.rect(manager.offsetX + manager.width - 250 - 5, manager.offsetY + 15, 250, 50);
+            // p.line(manager.offsetX + manager.width, manager.offsetY, manager.offsetX + manager.width, manager.offsetY + manager.height);
+            // p.line(manager.offsetX, manager.offsetY + manager.height, manager.offsetX, manager.offsetY);
+
+            if(this.clickedCircle != null) {
+                p.fill('black');
+                p.noStroke();
+                p.textAlign(p.CENTER, p.CENTER);
+                p.text(this.clickedCircle.name, manager.offsetX + manager.width - 125 - 5, manager.offsetY + 40)
+                p.textAlign(p.LEFT, p.BASELINE);
+                if (p.dist(p.mouseX, p.mouseY, this.clickedCircle.x, this.clickedCircle.y) > (this.clickedCircle.r / 2 + 5)) {
+                    this.clickedCircle = null;
+                }
+            }
+
+
+            // let bigRadius = manager.height * 0.46 - 2;
+            // let maxSpacing = Math.sqrt(p.PI * bigRadius * bigRadius / this.maxPlayers);
+            // let r = maxSpacing * 0.7;
+            // console.log(maxSpacing)
+            // let spacing = maxSpacing;
+            // let maxCountRows = Math.floor(bigRadius * 2 / spacing) //get the max amount of rows possible with spacing
+            // let minSpacingY = bigRadius * 2 / maxCountRows;
+            // p.randomSeed(999);
+            // let gap = spacing - r;
+            // let count = 0;
+            // for(let row = 0; row <= maxCountRows; row++) {
+            //     let y = minSpacingY / 2 + row * minSpacingY - bigRadius;
+            //     let maxX = Math.sqrt(bigRadius * bigRadius - y * y);
+            //     let maxCountCols = Math.floor(maxX * 2 / spacing);
+            //     let minSpacingX = (maxCountCols != 0 ? maxX * 2 / maxCountCols : 0);
+            //     for(let i = 0; i <= maxCountCols; i++) {
+            //         if(count + 1 > this.maxPlayers) break;
+            //         p.circle(midX - maxX + (minSpacingX * i) + p.random(-gap, gap), midY - y + p.random(-gap, gap), r);
+            //         count++;
+            //     }
+            // }
+
+
             p.noStroke();
             p.fill('black');
-        }
-
+        },
     };
 
 })();

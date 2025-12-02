71ddeb397a874b057b44397d59bdff132cac01af
ltsan-UW <ltsan30@uw.edu>
2025-12-01 19:25:32 -0800

    Moved VORP definition from performance circle viz to top 10 viz

--- js/sketches/viz/viz_allplayers.js
+++ js/sketches/viz/viz_allplayers.js
@@ -105,38 +105,6 @@
             this.drawHeader(p, manager.currentSeason, manager, this.maxPlayers);
 
 
-
-            // p.circle(manager.offsetX + manager.width / 2, manager.offsetY + manager.height / 2, bigRadius * 2);
-
-            // p.line(xStart, yEnd, xStart, yStart);
-            // p.line(xEnd, yEnd, xEnd, yStart);
-
-            //let pixelsToMeter = xDistance / 13.42;
-
-            // let yStart = midY - bigRadius;
-            // let yEnd = midY + bigRadius + 20;
-            // let xStart = manager.offsetX;
-            // let xEnd = manager.offsetX + manager.width;
-
-            // let xDistance = xEnd - xStart;
-            // let pixelsToMeter = xDistance / 15;
-            // p.strokeWeight(2);
-            // p.stroke('grey')
-            // p.noFill();
-            // p.line(xStart, yEnd, xEnd, yEnd);
-            // // p.line(xStart, yEnd, xStart, yEnd - 10 * pixelsToMeter);
-            // // p.line(xEnd, yEnd, xEnd, yEnd - 10 * pixelsToMeter);
-            // p.line(xStart + pixelsToMeter * 0.9, yEnd, xStart + pixelsToMeter * 0.9, yEnd - 3.04* pixelsToMeter);
-            // p.line(xEnd - pixelsToMeter * 0.9, yEnd, xEnd - pixelsToMeter * 0.9, yEnd - 3.04 * pixelsToMeter);
-            // p.arc(midX, yEnd - 3.04 * pixelsToMeter, xDistance - pixelsToMeter * 0.9 * 2, 5.5 * pixelsToMeter * 2, p.PI, p.TWO_PI)
-            // p.line(midX - 4.9 * pixelsToMeter / 2, yEnd, midX - 4.9 * pixelsToMeter / 2, yEnd - 4.6 * pixelsToMeter);
-            // p.line(midX + 4.9 * pixelsToMeter / 2, yEnd, midX + 4.9 * pixelsToMeter / 2, yEnd - 4.6 * pixelsToMeter);
-            // p.line(midX - 4.9 * pixelsToMeter / 2, yEnd - 4.6 * pixelsToMeter, midX + 4.9 * pixelsToMeter / 2, yEnd - 4.6 * pixelsToMeter);
-            // p.arc(midX, yEnd - 4.6 * pixelsToMeter, 3.65 * pixelsToMeter, 1.8 * pixelsToMeter * 2, p.PI, p.TWO_PI)
-            // p.stroke('lightgrey')
-            // p.arc(midX, yEnd - 4.6 * pixelsToMeter, 3.65 * pixelsToMeter, 1.8 * pixelsToMeter * 2, p.TWO_PI, p.PI)
-
-
             p.strokeWeight(1);
             p.stroke('grey')
             p.fill('lightgrey');
